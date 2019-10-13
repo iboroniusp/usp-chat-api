38f0a55f27e3626dc4bae309480b5b41ca19a913
Vitor Paes <paesvitor@outlook.com>
2019-10-13 12:41:41 -0300

    Update process port - LIBERTE AS PORTAS

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const routes = require("./routes");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const port = process.env.PORT || 3005;
 
 var app = require("express")();
 var http = require("http").createServer(app);
@@ -32,4 +33,4 @@ mongoose.connect(dbUrl, {
 });
 
 app.use(routes);
-http.listen(3005);
+http.listen(port);
